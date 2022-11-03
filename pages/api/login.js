73db0d3a1857f7ef850be9cc59d00fe3e1d40a05
Daniel Popescu <same.dan@gmail.com>
2022-11-03 12:37:05 +0100

    Added Middleware with joseJWT

--- pages/api/login.js
+++ pages/api/login.js
@@ -10,12 +10,13 @@ export default async function login(req, res) {
       const didToken = auth ? auth.substr(7) : "";
 
       const metadata = await magicAdmin.users.getMetadataByToken(didToken);
+      console.log("metadata ");
       console.log({ metadata });
 
-      const token = jwt.sign(
+      const token = await jwt.sign(
         {
           ...metadata,
-          iat: Math.floor(Date.now() / 1000 - 24 * 60 * 60),
+          iat: Math.floor(Date.now() / 1000 - 1 * 60 * 60),
           exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
           "https://hasura.io/jwt/claims": {
             "x-hasura-allowed-roles": ["user", "admin"],
